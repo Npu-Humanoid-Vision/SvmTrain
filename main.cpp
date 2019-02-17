461da1c15b79b292002ffa8be6057ea06915ec4e
Alex_Beng <599267065@qq.com>
2019-02-17 21:16:27 +0800

    recositution done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include "HogGetter.h"
 #include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
@@ -7,54 +8,127 @@ using namespace cv;
 #define POS_LABLE 1
 #define NEG_LABLE 0
 
-void GetXsSampleData(const string& folder_path, const string& postfix, int lable, 
-            cv::Mat& train_data, cv::Mat& train_data_lables) {
-    
-    HogGetter hog_getter;
-    hog_getter.set_window_size(cv::Size(32,32));
-    hog_getter.ImageReader_(folder_path, postfix);
-    hog_getter.HogComputter_();
+#ifdef __linux__
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#endif
 
-    // train_data = hog_getter.sample_features_.clone();
-    train_data.push_back(hog_getter.sample_features_.clone());
-    for (int i = 0; i < hog_getter.sample_nums_; i++) { // 
+#ifdef __WIN32
+#include <io.h>
+#include <windows.h>
+#endif
+
+// 获得某文件夹下所有图片的名字
+void GetImgNames(string root_path, std::vector<std::string>& names) {
+#ifdef __linux__
+    struct dirent* filename;
+    DIR* dir;
+    dir = opendir(root_path.c_str());  
+    if(NULL == dir) {  
+        return;  
+    }  
+
+    int iName=0;
+    while((filename = readdir(dir)) != NULL) {  
+        if( strcmp( filename->d_name , "." ) == 0 ||
+            strcmp( filename->d_name , "..") == 0)
+            continue;
+
+        string t_s(filename->d_name);
+        names.push_back(t_s);
+    }
+#endif
+
+#ifdef __WIN32
+    intptr_t hFile = 0;
+    struct _finddata_t fileinfo;
+    string p;
+
+    hFile = _findfirst(p.assign(root_path).append("/*").c_str(), &fileinfo);
+
+    if (hFile != -1) {
+        do {
+            if (strcmp(fileinfo.name, ".") == 0 || strcmp(fileinfo.name, "..") == 0) {
+                continue;
+            }
+            names.push_back(fileinfo.name); 
+        } while (_findnext(hFile, &fileinfo) == 0);
+    }
+#endif
+}
+
+
+void GetXsSampleData(const string folder_path, int lable, 
+            cv::Mat& train_data, cv::Mat& train_data_lables) {
+
+    // get the image names
+    std::vector<std::string> image_names;
+    GetImgNames(folder_path, image_names);
+    cout<<image_names.size()<<endl;
+    // define hog descriptor 
+    cv::HOGDescriptor hog_des(Size(32, 32), Size(16, 16), Size(8, 8), Size(8, 8), 9);
+
+    // read images and compute
+    for (auto i = image_names.begin(); i != image_names.end(); i++) {
+        string t_path = folder_path + (*i);
+        cv::Mat t_image = cv::imread(t_path);
+        std::vector<float> t_descrip_vec;
+        hog_des.compute(t_image, t_descrip_vec);
+
+        // initialization for train data
+        if (i == image_names.begin()) {
+            train_data = cv::Mat::zeros(image_names.size(), t_descrip_vec.size(), CV_32FC1);
+        } 
+        // copy t_descrip_vec to train_data
+        for (auto j = 0; j < t_descrip_vec.size(); j++) {
+            train_data.at<float>(i-image_names.begin(), j) = t_descrip_vec[j];
+        }
         train_data_lables.push_back(lable);
     }
 }
 
 int main(int argc, char const *argv[]) {
+#ifdef __WIN32 // mingw 只配了 opencv2
+    
+#endif
 
-    string root_path = "../../BackUpSource/Ball/Train/";
-    cv::Mat train_data;
-    cv::Mat train_data_lables;
+#ifdef __linux__
 
-    GetXsSampleData( root_path + "Pos/", "*.jpg", POS_LABLE, train_data, train_data_lables );
-    GetXsSampleData( root_path + "Neg/", "*.jpg", NEG_LABLE, train_data, train_data_lables );
-    // cout<<train_data.size()<<' '<<train_data.type()<<endl;
-    // cout<<train_data_lables.size()<<' '<<train_data_lables.type()<<endl;
-    // cout<<train_data.size()<<endl;
-    // cout<<train_data_lables.size()<<endl;
+#endif
 
-    // 参数设置
-    CvSVMParams train_params;
-    train_params.svm_type = CvSVM::C_SVC;
-    train_params.kernel_type = CvSVM::RBF;
-    train_params.degree = 0;
-    train_params.gamma = 1;
-    train_params.coef0 = 0;
-    train_params.C = 1;
-    train_params.nu = 0;
-    train_params.p = 0;
-    train_params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 100000, 1e-7); // 训练终止条件
+    // GetXsSampleData( root_path + "Pos/", "*.jpg", POS_LABLE, train_data, train_data_lables );
+    // GetXsSampleData( root_path + "Neg/", "*.jpg", NEG_LABLE, train_data, train_data_lables );
+    // // cout<<train_data.size()<<' '<<train_data.type()<<endl;
+    // // cout<<train_data_lables.size()<<' '<<train_data_lables.type()<<endl;
+    // // cout<<train_data.size()<<endl;
+    // // cout<<train_data_lables.size()<<endl;
 
-    CvSVM trainer;
-    // trainer.train(train_data, train_data_lables, cv::Mat(), cv::Mat(), train_params);
-    // trainer.train()
-    trainer.train_auto(train_data, train_data_lables, cv::Mat(), cv::Mat(), train_params);
+    // // 参数设置
+    // CvSVMParams train_params;
+    // train_params.svm_type = CvSVM::C_SVC;
+    // train_params.kernel_type = CvSVM::RBF;
+    // train_params.degree = 0;
+    // train_params.gamma = 1;
+    // train_params.coef0 = 0;
+    // train_params.C = 1;
+    // train_params.nu = 0;
+    // train_params.p = 0;
+    // train_params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 100000, 1e-7); // 训练终止条件
 
-    cout<<trainer.params.C<<trainer.params.nu<<endl;
-    cout<<"train done"<<endl;
-    trainer.save("ball_linear_auto.xml");
+    // CvSVM trainer;
+    // // trainer.train(train_data, train_data_lables, cv::Mat(), cv::Mat(), train_params);
+    // // trainer.train()
+    // trainer.train_auto(train_data, train_data_lables, cv::Mat(), cv::Mat(), train_params);
+
+    // cout<<trainer.params.C<<trainer.params.nu<<endl;
+    // cout<<"train done"<<endl;
+    // trainer.save("ball_linear_auto.xml");
 
     return 0;
 }
