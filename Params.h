d1d8d82757927b8f68595ae4f4aa97dfc648197f
Alex_Beng <599267065@qq.com>
2019-04-14 21:01:13 +0800

    simplify code by updating Params.h

--- Params.h
+++ Params.h
@@ -1,9 +1,102 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
 #define POS_LABLE 1
 #define NEG_LABLE 0
 
-#define MODEL_NAME "./model/BigBall/nu_svc_sigmoid.xml"
+#ifdef __linux__
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#endif
+
+#ifdef __WIN32
+#include <io.h>
+#include <windows.h>
+#endif
+
+
+#define MODEL_NAME "./model/BigBall/c_svc_with_moment.xml"
 
 #define TESTSET_PATH "../../BackUpSource/BigBall/Test/"
 #define TRAINSET_PATH "../../BackUpSource/BigBall/Train/"
+
+
+// 获得某文件夹下所有图片的名字
+void GetImgNames(string root_path, std::vector<std::string>& names) {
+#ifdef __linux__
+    struct dirent* filename;
+    DIR* dir;
+    dir = opendir(root_path.c_str());  
+    if(NULL == dir) {  
+        return;  
+    }  
+
+    int iName=0;
+    while((filename = readdir(dir)) != NULL) {  
+        if( strcmp( filename->d_name , "." ) == 0 ||
+            strcmp( filename->d_name , "..") == 0)
+            continue;
+
+        string t_s(filename->d_name);
+        names.push_back(t_s);
+    }
+#endif
+
+#ifdef __WIN32
+    intptr_t hFile = 0;
+    struct _finddata_t fileinfo;
+    string p;
+
+    hFile = _findfirst(p.assign(root_path).append("/*").c_str(), &fileinfo);
+
+    if (hFile != -1) {
+        do {
+            if (strcmp(fileinfo.name, ".") == 0 || strcmp(fileinfo.name, "..") == 0) {
+                continue;
+            }
+            names.push_back(fileinfo.name); 
+        } while (_findnext(hFile, &fileinfo) == 0);
+    }
+#endif
+}
+
+
+void GetXsSampleData(const string folder_path, int lable, 
+            cv::Mat& train_data, cv::Mat& train_data_lables) {
+
+    // get the image names
+    std::vector<std::string> image_names;
+    GetImgNames(folder_path, image_names);
+
+    // define hog descriptor 
+    cv::HOGDescriptor hog_des(Size(128, 128), Size(16, 16), Size(8, 8), Size(8, 8), 9);
+
+    // read images and compute
+    for (auto i = image_names.begin(); i != image_names.end(); i++) {
+        string t_path = folder_path + (*i);
+        cv::Mat t_image = cv::imread(t_path);
+        std::vector<float> t_descrip_vec;
+        hog_des.compute(t_image, t_descrip_vec);
+
+
+        // copy t_descrip_vec to train_data
+        cv::Mat t_mat = cv::Mat(1, t_descrip_vec.size(), CV_32FC1);
+        for (auto j = 0; j < t_descrip_vec.size(); j++) {
+            t_mat.at<float>(0, j) = t_descrip_vec[j];
+        }
+        train_data.push_back(t_mat);
+        train_data_lables.push_back(lable);
+    }
+}
 
 // #define REGRESSION
--- Train.cpp
+++ Train.cpp
@@ -1,97 +1,5 @@
 #include "Params.h"
 
-#include <bits/stdc++.h>
-#include <vector>
-#include <iostream>
-#include <opencv2/opencv.hpp>
-using namespace std;
-using namespace cv;
-
-
-#ifdef __linux__
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <string.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#endif
-
-#ifdef __WIN32
-#include <io.h>
-#include <windows.h>
-#endif
-
-// 获得某文件夹下所有图片的名字
-void GetImgNames(string root_path, std::vector<std::string>& names) {
-#ifdef __linux__
-    struct dirent* filename;
-    DIR* dir;
-    dir = opendir(root_path.c_str());  
-    if(NULL == dir) {  
-        return;  
-    }  
-
-    int iName=0;
-    while((filename = readdir(dir)) != NULL) {  
-        if( strcmp( filename->d_name , "." ) == 0 ||
-            strcmp( filename->d_name , "..") == 0)
-            continue;
-
-        string t_s(filename->d_name);
-        names.push_back(t_s);
-    }
-#endif
-
-#ifdef __WIN32
-    intptr_t hFile = 0;
-    struct _finddata_t fileinfo;
-    string p;
-
-    hFile = _findfirst(p.assign(root_path).append("/*").c_str(), &fileinfo);
-
-    if (hFile != -1) {
-        do {
-            if (strcmp(fileinfo.name, ".") == 0 || strcmp(fileinfo.name, "..") == 0) {
-                continue;
-            }
-            names.push_back(fileinfo.name); 
-        } while (_findnext(hFile, &fileinfo) == 0);
-    }
-#endif
-}
-
-
-void GetXsSampleData(const string folder_path, int lable, 
-            cv::Mat& train_data, cv::Mat& train_data_lables) {
-
-    // get the image names
-    std::vector<std::string> image_names;
-    GetImgNames(folder_path, image_names);
-
-    // define hog descriptor 
-    cv::HOGDescriptor hog_des(Size(128, 128), Size(16, 16), Size(8, 8), Size(8, 8), 9);
-
-    // read images and compute
-    for (auto i = image_names.begin(); i != image_names.end(); i++) {
-        string t_path = folder_path + (*i);
-        cv::Mat t_image = cv::imread(t_path);
-        std::vector<float> t_descrip_vec;
-        hog_des.compute(t_image, t_descrip_vec);
-
-
-        // copy t_descrip_vec to train_data
-        cv::Mat t_mat = cv::Mat(1, t_descrip_vec.size(), CV_32FC1);
-        for (auto j = 0; j < t_descrip_vec.size(); j++) {
-            t_mat.at<float>(0, j) = t_descrip_vec[j];
-        }
-        train_data.push_back(t_mat);
-        train_data_lables.push_back(lable);
-    }
-}
-
 int main(int argc, char const *argv[]) {
     // for train time
     double begin;
@@ -109,7 +17,7 @@ int main(int argc, char const *argv[]) {
     // 参数设置
     CvTermCriteria criteria = cvTermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 50000, FLT_EPSILON);
     //SVM参数：SVM类型为C_SVC；线性核函数；松弛因子C=0.01
-    CvSVMParams train_params(CvSVM::NU_SVR, CvSVM::SIGMOID, 0.1, 0.1, 0.1, 0.1, 0.5, 0.5, 0, criteria);
+    CvSVMParams train_params(CvSVM::C_SVC, CvSVM::LINEAR, 0.1, 0.1, 0.1, 1, 0.5, 0.5, 0, criteria);
 
     CvSVM trainer;
 
