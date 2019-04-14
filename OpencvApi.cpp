4e328b5905a507035657e9063aa4d0894deb70ed
Alex Beng <599267065@qq.com>
2019-04-15 00:52:54 +0800

    update opencv-api version & add elbp descriptor

--- OpencvApi.cpp
+++ OpencvApi.cpp
@@ -2,5 +2,59 @@
 
 
 int main() {
+    string pos_root_path = string(TRAINSET_PATH) + string("Pos/");
+    string neg_root_path = string(TRAINSET_PATH) + string("Neg/");
+
+    std::vector<std::string> pos_paths; 
+    std::vector<std::string> neg_paths;
+    GetImgNames(pos_root_path, pos_paths);
+    GetImgNames(neg_root_path, neg_paths);
+
+    std::vector<cv::Mat> image;
+    std::vector<int> lable;
+
+    for (auto i = pos_paths.begin(); i != pos_paths.end(); i++) {
+        // cout<<pos_root_path+*i<<endl;
+        cv::Mat t_image = cv::imread(pos_root_path+*i);
+        t_image = GetUsedChannel(t_image, L);
+        image.push_back(t_image);
+        lable.push_back(POS_LABLE);
+    }
+    for (auto i = neg_paths.begin(); i != neg_paths.end(); i++) {
+        cv::Mat t_image = cv::imread(neg_root_path+*i);
+        t_image = GetUsedChannel(t_image, L);
+        image.push_back(t_image);        
+        lable.push_back(NEG_LABLE);
+    }
+
+    Ptr<FaceRecognizer> model = createLBPHFaceRecognizer();
+	model->train(image, lable);
+
+
+    pos_paths.resize(0);
+    neg_paths.resize(0);
+    pos_root_path = string(TESTSET_PATH) + string("Pos/");
+    neg_root_path = string(TESTSET_PATH) + string("Neg/");
+
+    GetImgNames(pos_root_path, pos_paths);
+    GetImgNames(neg_root_path, neg_paths);
+
+    int correct_sum = 0;
+    for (auto i = pos_paths.begin(); i != pos_paths.end(); i++) {
+        // cout<<pos_root_path+*i<<endl;
+        cv::Mat t_image = cv::imread(pos_root_path+*i);
+        t_image = GetUsedChannel(t_image, L);
+        correct_sum += model->predict(t_image);
+    }
+    cout<<correct_sum*1.0/pos_paths.size()<<endl;
+
+    correct_sum = 0;
+    for (auto i = neg_paths.begin(); i != neg_paths.end(); i++) {
+        cv::Mat t_image = cv::imread(neg_root_path+*i);
+        t_image = GetUsedChannel(t_image, L);
+        correct_sum += model->predict(t_image);
+    } 
+    cout<<1 - correct_sum*1.0/neg_paths.size()<<endl;
+
     return 0;
 }
--- Params.h
+++ Params.h
@@ -32,12 +32,17 @@ using namespace cv;
 #define TRAINSET_PATH "../../BackUpSource/BigBall/Train/"
 
 
+// #define REGRESSION
+
 enum { H,S,V,L,A,B };
 
 void GetImgNames(string root_path, std::vector<std::string>& names);
 cv::Mat GetUsedChannel(cv::Mat& src_img, int flag);
 void GetXsSampleData(const string folder_path, int lable, 
             cv::Mat& train_data, cv::Mat& train_data_lables);
+template <typename _Tp> static
+inline void elbp_(InputArray _src, OutputArray _dst, int radius, int neighbors);
+static void elbp(InputArray src, OutputArray dst, int radius, int neighbors);
 
 // 获得某文件夹下所有图片的名字
 void GetImgNames(string root_path, std::vector<std::string>& names) {
@@ -119,18 +124,28 @@ void GetXsSampleData(const string folder_path, int lable,
         // hog related
         hog_des.compute(t_image, t_descrip_vec);
 
-        // moment related
+        // moment&lbp related
         for (int j=0; j<6; j++) {
             cv::Mat t_image_l = GetUsedChannel(t_image, j);
             cv::Moments moment = cv::moments(t_image_l, false);
+
+            cv::Mat lbp_vec;
+            elbp(t_image_l, lbp_vec, 1, 8);
+            lbp_vec = lbp_vec.reshape(1, 1);
+            // cv::imshow("yayaya", t_image_l);
+            // cv::waitKey();
+            // cout<<lbp_vec.size()<<endl;
+
             double hu[7];
             cv::HuMoments(moment, hu);
             for (int k=0; k<7; k++) {
                 t_descrip_vec.push_back(hu[k]);
             }
+            for (int k=0; k<lbp_vec.cols; k++) {
+                t_descrip_vec.push_back(lbp_vec.at<uchar>(0, k));
+            }
         }
-        Ptr<FaceRecognizer> model = createLBPHFaceRecognizer();
-
+        
 
         // copy t_descrip_vec to train_data
         cv::Mat t_mat = cv::Mat(1, t_descrip_vec.size(), CV_32FC1);
@@ -142,4 +157,58 @@ void GetXsSampleData(const string folder_path, int lable,
     }
 }
 
-// #define REGRESSION
+template <typename _Tp> static
+inline void elbp_(InputArray _src, OutputArray _dst, int radius, int neighbors) {
+    //get matrices
+    Mat src = _src.getMat();
+    // allocate memory for result
+    _dst.create(src.rows-2*radius, src.cols-2*radius, CV_32SC1);
+    Mat dst = _dst.getMat();
+    // zero
+    dst.setTo(0);
+    for(int n=0; n<neighbors; n++) {
+        // sample points
+        float x = static_cast<float>(radius * cos(2.0*CV_PI*n/static_cast<float>(neighbors)));
+        float y = static_cast<float>(-radius * sin(2.0*CV_PI*n/static_cast<float>(neighbors)));
+        // relative indices
+        int fx = static_cast<int>(floor(x));
+        int fy = static_cast<int>(floor(y));
+        int cx = static_cast<int>(ceil(x));
+        int cy = static_cast<int>(ceil(y));
+        // fractional part
+        float ty = y - fy;
+        float tx = x - fx;
+        // set interpolation weights
+        float w1 = (1 - tx) * (1 - ty);
+        float w2 =      tx  * (1 - ty);
+        float w3 = (1 - tx) *      ty;
+        float w4 =      tx  *      ty;
+        // iterate through your data
+        for(int i=radius; i < src.rows-radius;i++) {
+            for(int j=radius;j < src.cols-radius;j++) {
+                // calculate interpolated value
+                float t = static_cast<float>(w1*src.at<_Tp>(i+fy,j+fx) + w2*src.at<_Tp>(i+fy,j+cx) + w3*src.at<_Tp>(i+cy,j+fx) + w4*src.at<_Tp>(i+cy,j+cx));
+                // floating point precision, so check some machine-dependent epsilon
+                dst.at<int>(i-radius,j-radius) += ((t > src.at<_Tp>(i,j)) || (std::abs(t-src.at<_Tp>(i,j)) < std::numeric_limits<float>::epsilon())) << n;
+            }
+        }
+    }
+}
+
+static void elbp(InputArray src, OutputArray dst, int radius, int neighbors)
+{
+    int type = src.type();
+    switch (type) {
+    case CV_8SC1:   elbp_<char>(src,dst, radius, neighbors); break;
+    case CV_8UC1:   elbp_<unsigned char>(src, dst, radius, neighbors); break;
+    case CV_16SC1:  elbp_<short>(src,dst, radius, neighbors); break;
+    case CV_16UC1:  elbp_<unsigned short>(src,dst, radius, neighbors); break;
+    case CV_32SC1:  elbp_<int>(src,dst, radius, neighbors); break;
+    case CV_32FC1:  elbp_<float>(src,dst, radius, neighbors); break;
+    case CV_64FC1:  elbp_<double>(src,dst, radius, neighbors); break;
+    default:
+        string error_msg = format("Using Original Local Binary Patterns for feature extraction only works on single-channel images (given %d). Please pass the image data as a grayscale image!", type);
+        CV_Error(CV_StsNotImplemented, error_msg);
+        break;
+    }
+}
